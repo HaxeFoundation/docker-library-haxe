320dd1019d7b3191b3bcc58efc14d4b2e5407f1b
Andy Li <andy@onthewings.net>
2019-09-05 14:53:53 +0800

    haxe 4.0.0-rc.4

--- Update.hx
+++ Update.hx
@@ -3,7 +3,7 @@ import haxe.io.*;
 import sys.*;
 import sys.io.*;
 
-typedef Version = {version:String, tag:String, sha256:Dynamic, win64:Bool, exclude:Array<String>, opam:Bool};
+typedef Version = {version:String, tag:String, sha256:Dynamic, win64:Bool, nekowin64:Bool, exclude:Array<String>, opam:Bool};
 typedef Variant = {variant:String, suffix:Array<String>};
 
 class Update {
@@ -20,6 +20,7 @@ class Update {
 			"version": "3.4.7",
 			"tag": "3.4.7",
 			"win64": true,
+			"nekowin64": false,
 			"sha256": {"win": "609acdcb58a2253e357487d495ffe19e9034165f3102f8716ca968afbee8f1b2"},
 			"exclude": [],
 			"opam": false
@@ -28,6 +29,7 @@ class Update {
 			"version": "3.3.0-rc.1",
 			"tag": "3.3.0-rc1",
 			"win64": false,
+			"nekowin64": false,
 			"sha256": {"win": "fa51621132432328a47e5e0416ab3b9f2f734b217a2bc9b650826aae2f12c6f4"},
 			"exclude": [],
 			"opam": false
@@ -36,6 +38,7 @@ class Update {
 			"version": "3.2.1",
 			"tag": "3.2.1",
 			"win64": false,
+			"nekowin64": false,
 			"sha256": {"win": "af57d42ca474bba826426e9403b2cb21c210d56addc8bbc0e8fafa88b3660db3"},
 			"exclude": [],
 			"opam": false
@@ -44,15 +47,17 @@ class Update {
 			"version": "3.1.3",
 			"tag": "3.1.3",
 			"win64": false,
+			"nekowin64": false,
 			"sha256": {"win": "4cf84cdbf7960a61ae70b0d9166c6f9bde16388c3b81e54af91446f4c9e44ae4"},
 			"exclude": ["alpine3.7", "alpine3.8", "alpine3.9", "alpine3.10", "buster"],
 			"opam": false
 		},
 		{
-			"version": "4.0.0-rc.3",
-			"tag": "4.0.0-rc.3",
+			"version": "4.0.0-rc.4",
+			"tag": "4.0.0-rc.4",
 			"win64": true,
-			"sha256": {"win": "ab10b6d2fcff6637aa3481bc682cb0f18c3f204134afa4a16941b642d693f29e"},
+			"nekowin64": true,
+			"sha256": {"win": "5ae553009c5ca22ecf2026ac27a9fae97bbaed549032eecbc6c783e347fc8372"},
 			"exclude": [],
 			"opam": true
 		},
@@ -153,10 +158,14 @@ class Update {
 					NEKO_TAG: "v2-2-0",
 					NEKO_SHA256: switch(variant.variant) {
 						case "windowsservercore-1809"|"windowsservercore-1803"|"windowsservercore-ltsc2016"|"nanoserver":
-							"93d7ca96698a6825f38ca8eea49e2e6b691c0849270174f6c1bd531290db8d69";
+							if (version.nekowin64)
+								"913d90223399088fd38c721f7b5e5a7d615326f8edf26e5fc3b2959e5945939d"
+							else
+								"93d7ca96698a6825f38ca8eea49e2e6b691c0849270174f6c1bd531290db8d69";
 						case _:
 							"cf101ca05db6cb673504efe217d8ed7ab5638f30e12c5e3095f06fa0d43f64e3";
 					},
+					NEKO_WIN: version.nekowin64 ? "win64" : "win",
 					HEADER: HEADER,
 					USE_OPAM: version.opam,
 				};
